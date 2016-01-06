b2c9f39840b2003ad6ce3c59637db3a9c39cacdf
awasthi <awasthi@psit.in>
2016-01-07 02:08:00 +0530

    Initial commit

---
+++ app/src/main/java/in/darbose/opengbu/gbutalktoall/AnnouncementFragment.java
@@ -0,0 +1,9 @@
+package in.darbose.opengbu.gbutalktoall;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by awasthi on 1/7/2016.
+ */
+public class AnnouncementFragment extends Fragment {
+}
